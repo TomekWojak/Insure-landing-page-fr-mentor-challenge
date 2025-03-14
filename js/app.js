1ddad609c8d753748ff01b08a47f490caaa322a0
Tomek <tomek.wojak796@gmail.com>
2025-03-14 23:16:12 +0100

    finished website

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 const burgerBtn = document.querySelector('.nav__burger-btn')
 const sideNavBar = document.querySelector('.nav__links-mobile')
+const navLinks = document.querySelectorAll('.nav__links-mobile .nav__link')
 
 
 burgerBtn.addEventListener('click', () => {
@@ -17,3 +18,8 @@ burgerBtn.addEventListener('click', () => {
     }
 
 })
+
+navLinks.forEach(link => link.addEventListener('click', () => {
+    sideNavBar.classList.remove('active')
+    burgerBtn.setAttribute('aria-expanded', 'false')
+}))
