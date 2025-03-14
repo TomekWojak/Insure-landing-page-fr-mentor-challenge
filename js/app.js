a421bad9db954476df37af36b5b2e13edfd393ed
Tomek <tomek.wojak796@gmail.com>
2025-03-14 17:04:48 +0100

    created header and nav on mobile - ready

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
 
 const burgerBtn = document.querySelector('.nav__burger-btn')
 const sideNavBar = document.querySelector('.nav__links-mobile')
-const body = document.body
-const html = document.documentElement
+
 
 burgerBtn.addEventListener('click', () => {
     sideNavBar.classList.toggle('active')
     const isOpened = burgerBtn.getAttribute('aria-expanded')
     burgerBtn.setAttribute('aria-expanded', 'true')
-    body.style.overflow = "hidden"
-    html.style.overflow = "hidden"
+
 
     if(isOpened === 'true'){
         burgerBtn.setAttribute('aria-expanded','false')
-        body.style.overflow = ""
-        html.style.overflow = ""
+
     }else {
         burgerBtn.setAttribute('aria-expanded','true')
     }
